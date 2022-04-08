a5471a15a480a55c1b3e6bef03de45bb66127a0b
alejaromerom <aleromero43@gmail.com>
2022-04-08 10:58:42 -0500

    funciones-express-nodejs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-const express = require ('express');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.listen(3000, () => {
+//settings
+app.set("port", process.env.PORT || 5000);
+//midlewares
+//routes
+//static files
+// starting the server
 
-
+app.listen(app.get("port"), () => {
+	console.log(` server on port ${app.get("port")}`);
 });
-
-
